eb94b4369d079d758ce583ba3852a68a47198125
hyoungnaoh <zzzsh789@naver.com>
2024-02-16 20:51:09 +0900

    Add: 일정 이벤트 당시 푸시알림 문구 수정 및 로직 리팩토링

--- src/batch/batch.service.ts
+++ src/batch/batch.service.ts
@@ -33,16 +33,23 @@ export class BatchService {
       date: current.format('YYYY-MM-DD HH:mm:ss'),
     });
 
+    const getScheduleFcmContent = (target: {
+      scheduleName: string;
+      phoneTokens: string[];
+      notification: number | null;
+    }) => {
+      if (target.notification === 0) {
+        return `${target.scheduleName} 일정 시간이에요!`;
+      }
+      if (target.notification === 60) {
+        return `${target.scheduleName} 일정이 1시간 뒤 예정되어 있어요!`;
+      }
+      return `${target.scheduleName} 일정이 ${target.notification}분 뒤 예정되어 있어요!`;
+    };
+
     schedulesWithPhoneTokens.forEach((target) => {
       target.phoneTokens.forEach((phoneToken) => {
-        this.fcmService.fcm(
-          phoneToken,
-          '플렘',
-          `${target.scheduleName} 일정이 ${
-            target.notification !== 60 ? `${target.notification}분 뒤` : '1시간 뒤'
-          } 예정되어 있어요!`,
-          'schedule'
-        );
+        this.fcmService.fcm(phoneToken, '플렘', getScheduleFcmContent(target), 'schedule');
       });
     });
   }
